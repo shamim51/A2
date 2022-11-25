eb86981745ed5fa1055a9898f3d663be9d12c286
Shamim Ahmed <shamimahmed30082@gmail.com>
2022-11-25 09:28:59 +0600

    completed form and signup file

--- singup.php
+++ singup.php
@@ -1,2 +1,27 @@
 <?php
-    $fname
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+
+    echo $fname;
+    echo " ";
+    echo $lname;
+    echo " ";
+
+    $link = mysqli_connect("localhost", "root", "", "formdb");
+
+    if($link == false){
+        die("ERROR: Could not connect. ". mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO userinfo(firstName, lastName) Values('$fname', '$lname')";
+    if(mysqli_query($link, $sql)){
+        echo "Records added successfully.";
+    }
+    else{
+        echo "ERROR: Could not able to excute $sql". mysql_error($link);
+
+    }
+
+    mysqli_close($link);
+
+    ?>
